c2d754aad70dd26597b14e03a0d48491243dba93
Roman M <prominb@gmail.com>
2024-01-21 03:46:11 +0200

    implement add_record in AddressBook

--- main.py
+++ main.py
@@ -13,20 +13,79 @@ class Name(Field):
     pass  # реалізація класу
 
 
-class Phone(Field):
+class Phone(Field):  # Реалізовано валідацію номера телефону (має бути 10 цифр).
     pass  # реалізація класу
 
 
 class Record:
+    '''Record: Додавання телефонів. Видалення телефонів. Редагування телефонів. Пошук телефону.'''
     def __init__(self, name):
-        self.name = Name(name)
-        self.phones = []
+        self.name = Name(name)  # Реалізовано зберігання об'єкта Name в окремому атрибуті.
+        self.phones = []  # Реалізовано зберігання списку об'єктів Phone в окремому атрибуті.
 
-    # реалізація класу
+    # Реалізовано методи для:
+    def add_phone(self):  # додавання - add_phone
+        pass
+
+    def remove_phone(self):  # видалення - remove_phone
+        pass
+
+    def edit_phone(self):  # редагування - edit_phone
+        pass
+
+    def find_phone(self):  # пошуку об'єктів Phone - find_phone
+        pass
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 
 class AddressBook(UserDict):
-    pass  # реалізація класу
+    '''AddressBook: Додавання записів. Пошук записів за іменем. Видалення записів за іменем.'''
+    """Записи Record у AddressBook зберігаються як значення у словнику. В якості ключів використовується значення Record.name.value."""
+    # Реалізовано метод add_record, який додає запис до self.data.
+    def add_record(self, record: Record):  # Додавання запису.
+        self.data[record.name.value] = record
+
+    def find(self):  # Реалізовано метод find, який знаходить запис за ім'ям.
+        pass
+
+    def delete(self):  # Реалізовано метод delete, який видаляє запис за ім'ям.
+        pass
+
+
+# Після реалізації ваш код має виконуватися наступним чином:
+
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+print(john_record)
+# john_record.add_phone("1234567890")
+# john_record.add_phone("5555555555")
+
+# # Додавання запису John до адресної книги
+# book.add_record(john_record)
+
+# # Створення та додавання нового запису для Jane
+# jane_record = Record("Jane")
+# jane_record.add_phone("9876543210")
+# book.add_record(jane_record)
+
+# # Виведення всіх записів у книзі
+# for name, record in book.data.items():
+#     print(record)
+
+# # Знаходження та редагування телефону для John
+# john = book.find("John")
+# john.edit_phone("1234567890", "1112223333")
+
+# print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# # Пошук конкретного телефону у записі John
+# found_phone = john.find_phone("5555555555")
+# print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+# # Видалення запису Jane
+# book.delete("Jane")
